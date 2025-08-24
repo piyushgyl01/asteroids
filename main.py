df86e7c7903bff1914edf71647278cb345d9d8b6
piyush goyal <piyush2022ug@gmail.com>
2025-08-24 12:27:41 +0530

    feat: grouping objects

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
@@ -19,11 +24,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
+        updatable.update(dt)
         
         screen.fill("black")
         
-        player.draw(screen)
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
         
         pygame.display.flip()
         
