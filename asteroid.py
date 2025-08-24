9d88534db298751011ec5286f95b984b4ba62777
piyush goyal <piyush2022ug@gmail.com>
2025-08-24 13:03:40 +0530

    feat: asteroids splitting

--- asteroid.py
+++ asteroid.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from circleshape import CircleShape
+from constants import ASTEROID_MIN_RADIUS
 
 
 class Asteroid(CircleShape):
@@ -14,3 +16,29 @@ class Asteroid(CircleShape):
     def update(self, dt):
         # Move in a straight line at constant speed
         self.position += self.velocity * dt
+    
+    def split(self):
+        # Always kill this asteroid first
+        self.kill()
+        
+        # If this is a small asteroid, we're done
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        # Generate a random angle between 20 and 50 degrees
+        random_angle = random.uniform(20, 50)
+        
+        # Create two new velocity vectors by rotating the current velocity
+        velocity1 = self.velocity.rotate(random_angle)
+        velocity2 = self.velocity.rotate(-random_angle)
+        
+        # Calculate the new radius for the smaller asteroids
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        
+        # Create two new asteroids at the current position
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        
+        # Set their velocities (scaled up by 1.2 to make them faster)
+        asteroid1.velocity = velocity1 * 1.2
+        asteroid2.velocity = velocity2 * 1.2
